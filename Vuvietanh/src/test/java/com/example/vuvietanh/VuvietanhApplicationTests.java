3d10eb2fe17ee4b22ba04921bbf1366c271099c9
Vũ Việt Anh <73330605+vabi1232@users.noreply.github.com>
2022-08-10 18:52:24 +0700

    up

---
+++ Vuvietanh/src/test/java/com/example/vuvietanh/VuvietanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vuvietanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuvietanhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
